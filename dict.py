76ed4d5d596f6e84a02dd7f40b1f49f1ff4900a4
gaurav <gauravkrishnajha2007@gmail.com>
2025-04-29 10:46:19 +0530

    dictinaries

---
+++ dict.py
@@ -0,0 +1,15 @@
+dic ={
+    1:"harry",
+    2:"gaurav",
+    3:"vishal",
+    4:"abhishek"
+     }
+print(dic[1])
+print(dic[2])
+print(dic[3])
+print(dic[4])
+
+
+for key in dic.keys():
+    print(dic[key])
+    
