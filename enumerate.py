985e7d05c41d5a2244c9f2d30ab4caea1411eb23
gaurav <gauravkrishnajha2007@gmail.com>
2025-04-30 19:33:32 +0530

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,15 @@
+# marks=[1,88,98,6,3,33]
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if(index==2):
+#         print("uracil,98")
+#     index +=1    
+
+marks=[1,88,98,6,3,33]
+
+for  index,mark in enumerate(marks):
+    print(mark)
+    if(index==2):
+        print("uracil")
+    
