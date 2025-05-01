4c141bc93a410c42a9d6b8491bb23d6c91820f5a
gaurav <gauravkrishnajha2007@gmail.com>
2025-05-01 11:09:51 +0530

    commiting unsved file

--- single program/os module.py
+++ single program/os module.py
@@ -1,6 +1,6 @@
-import os
-if ~os.path.exists("pqr"):
-    os.mkdir("pqr")
+# import os
+# if ~os.path.exists("pqr"):
+#     os.mkdir("pqr")
 
-for i in range (0,100):
-    os.mkdir(f"data/abc{i+1}")
+# for i in range (0,100):
+#     os.mkdir(f"data/abc{i+1}")
