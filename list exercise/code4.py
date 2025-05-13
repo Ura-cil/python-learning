eade8e78ef4c70aacaa9700f612154e22e45c00e
gaurav <gauravkrishnajha2007@gmail.com>
2025-05-13 10:00:08 +0530

    exercise list

---
+++ list exercise/code4.py
@@ -0,0 +1,12 @@
+# Exercise 4: Concatenate two lists in the following order
+# list1 = ["Hello ", "take "]
+# list2 = ["Dear", "Sir"]
+# Expected output:
+
+# ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
+
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+
+res = [x + y for x in list1 for y in list2]
+print(res)
